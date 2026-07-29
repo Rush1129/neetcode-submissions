25121788b6701a72fc8452b40aca83094ff6e539
Rushikesh Vekaria <159521688+Rush1129@users.noreply.github.com>
2026-07-29 22:01:15 +0530

    Add: maximum-product-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-2.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        maxe,mine=1,1
+        hs=nums[0]
+        for i in nums:
+            m= maxe*i
+            maxe = max(i*maxe,i*mine,i) 
+            mine = min(m,mine*i,i)
+            hs = max(hs,maxe)
+        return hs
